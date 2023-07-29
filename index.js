b6cef9be60eb482165ad4d1f170fb076e6b0a712
EstebanXavierCS <xaviercs.ti22@utsjr.edu.mx>
2023-07-28 20:51:57 -0600

    primer commit

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 var express = require("express");
 var path = require("path");
-var usuarioRutas = require("./rutas/usuario");
 const session = require("cookie-session");
-//const session = require("express-session");
+var usuarioRutas = require("./rutas/usuario");
 require("dotenv").config();
 
 var app = express();
 app.set("view engine", "ejs");
 app.use("/web", express.static(path.join(__dirname, "web")));
 app.use(express.urlencoded({ extended: true }));
-app.use("/", usuarioRutas);
 app.use(
   session({
     name:'session',
     keys:[process.env.SECRETO_SESSION]
-   /* secret: process.env.SECRETO_SESSION,
-    resave: true,
-    saveUninitialized: true,
-    */
   }));
+app.use("/", usuarioRutas);
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("Servidor en http://localhost:" + port);
+  console.log("Servidor en http://localhost:" + port)
 });
